967fb84e37c36814473df514b933ef8367dd79d1
karlo57 <karlo.karlo.karlo@gmail.com>
2013-03-31 09:49:40 +0800

    added add event button, minor UI changes, no db save yet

--- events.js
+++ events.js
@@ -1,15 +1,26 @@
 Events = new Meteor.Collection("events");
 
 if (Meteor.isClient) {
-    Template.page.events = function () {
-    return Events.find({}, {});
-  };
+
+  Template.page.showAddEventModule = function () {
+    return Session.get("addEvent");
+  }
 
   Template.event.events({
     'click #event' : function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
+    }
+  });
+
+  Template.eventSidebar.eventBlock = function () {
+    return Events.find({}, {});
+  };
+
+  Template.eventSidebar.events({
+    'click #addevent' : function(event) {
+      Session.set("addEvent", true);
     }
   });
 
