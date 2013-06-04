d0f0b8ec5633625197b4ce886d63ab17bcc0c204
Karlo <karlo@Karlos-MacBook-Air.local>
2013-06-04 10:42:09 +0800

    fixed week1

--- client/week1/js/week1.js
+++ client/week1/js/week1.js
@@ -91,53 +91,6 @@ Template.week1.events({
       toastr.success('Potential venues saved!', 'Week 1');
   });
 
-  table = template.find("#work_table");
-  records = _.rest($(table).find("tr"));
-  save = [];
-  $(records).each( function () {
-    var work = $($(this).find("a")[0]).html();
-    var num = $(this).find("a.editNum").html();
-    var editable = $(this).find("a.editWork").html();
-    if(!editable)
-    {
-      if(num == "Empty")
-      {
-        var push = {
-          name: work,
-        };
-      }
-      else
-      {
-        var push = {
-          name: work,
-          num: num
-        };
-      }
-      save.push(push);
-    }
-    else if($(this).find("a.editable-empty").length == 0)
-    {
-      var push = {
-        name: work,
-        num: num
-      };
-      save.push(push);
-    }
-  });
-  Meteor.call("updateWork",
-  {
-    work: save,
-    selected: Session.get("selected")
-  },
-  function (error, _id) {
-    if (error) {
-      toastr.error(error.details, error.reason);
-    }
-    else {
-      toastr.success('Work logistics info saved!', 'Week 1');
-    }    
-  }); 
-
   Meteor.call("updateText",
   {
     selected: Session.get("selected")
@@ -150,8 +103,8 @@ Template.week1.dates = function () {
   var event = Events.findOne({_id: Session.get("selected")});
   if(event)
   {
-    var startDate = moment(event.startTime).subtract('weeks', 5).format("ddd, MMM DD");
-    var endDate = moment(event.startTime).subtract('weeks', 4).format("ddd, MMM DD");
+    var startDate = moment(event.startTime).subtract('weeks', 6).format("ddd, MMM DD");
+    var endDate = moment(event.startTime).subtract('weeks', 5).format("ddd, MMM DD");
     return startDate + " - " + endDate;
   }
 }
