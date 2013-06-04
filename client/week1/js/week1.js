126d0a2e8042cfbf87f2bb7a7a124195cc5f21b0
Karlo <karlo@Karlos-MacBook-Air.local>
2013-06-04 10:42:09 +0800

    fixed week1

--- client/week1/js/week1.js
+++ client/week1/js/week1.js
@@ -150,8 +150,8 @@ Template.week1.dates = function () {
   var event = Events.findOne({_id: Session.get("selected")});
   if(event)
   {
-    var startDate = moment(event.startTime).subtract('weeks', 5).format("ddd, MMM DD");
-    var endDate = moment(event.startTime).subtract('weeks', 4).format("ddd, MMM DD");
+    var startDate = moment(event.startTime).subtract('weeks', 6).format("ddd, MMM DD");
+    var endDate = moment(event.startTime).subtract('weeks', 5).format("ddd, MMM DD");
     return startDate + " - " + endDate;
   }
 }
