ede35c72c35f277e43e9932b005da08bee480493
Karlo <karlo@Karlos-MacBook-Air.local>
2013-06-04 11:29:54 +0800

    fixed up directories finished up week4

--- client/week4/js/week4.js
+++ client/week4/js/week4.js
@@ -2,8 +2,8 @@ Template.week4.dates = function () {
   var event = Events.findOne({_id: Session.get("selected")});
   if(event)
   {
-    var startDate = moment(event.startTime).subtract('weeks', 2).format("ddd, MMM DD");
-    var endDate = moment(event.startTime).subtract('weeks', 1).format("ddd, MMM DD");
+    var startDate = moment(event.startTime).subtract('weeks', 3).format("ddd, MMM DD");
+    var endDate = moment(event.startTime).subtract('weeks', 2).format("ddd, MMM DD");
     return startDate + " - " + endDate;
   }
 }
@@ -11,24 +11,62 @@ Template.week4.dates = function () {
 Template.week4.events({
   'click #save' : function (event, template) {
     Session.set("loading", true)
-    var table = template.find("#remind_performers_table");
+
+    var table = template.find("#lineup_contract_table")
     var records = _.rest($(table).find("tr"));
     var save = [];
     $(records).each( function () {
-      var band = $(this).find("a.editPerformerRemind").html();
-      var status = $(this).find("a.editPerformerRemindStatus").html();
-      if(($(this).find("a.editable-empty").length == 0))
+      var band = $(this).find("a.lineupContract").html();
+      var status = $(this).find("a.editLineupContractStatus").html();
+      var date = $(this).find("a.editLineupContractDate").html();
+      if(($(this).find("a.lineupContract.editable-empty").length == 0))
       {
+        if(date == "Empty")
+          date = ""
         var push = {
           band: band,
-          status: status
+          status: status,
+          date: date
         };
         save.push(push);
       }
     });
-    Meteor.call("updatePerformerRemind",
+    if(save.length > 0)
     {
-      performerRemind: save,
+      Meteor.call("updateLineupContract",
+      {
+        wk2lineupContract: save,
+        selected: Session.get("selected")
+      },
+      function (error, _id) {
+        if (error) {
+          toastr.error(error.details, error.reason);
+        }
+        else {
+          toastr.success('Band contract info saved!', 'Week 4')
+        } 
+      });      
+    } 
+
+    table = template.find("#promotions_table");
+    records = _.rest($(table).find("tr"));
+    save = [];
+    $(records).each( function () {
+      var platform = $(this).find("a.editPlatform").html();
+      var link = $(this).find("a.editLink").html();
+      if(($(this).find("a.editable-empty").length == 0))
+      {
+        var push = {
+          platform: platform,
+          link: link
+        };
+        save.push(push);
+      }
+    });
+
+   Meteor.call("updatePromotions",
+    {
+      promotions: save,
       selected: Session.get("selected")
     },
     function (error, _id) {
@@ -36,123 +74,10 @@ Template.week4.events({
         toastr.error(error.details, error.reason)
       }
       else
-        toastr.success("Remind performers info saved!", "Week 4")
-    }); 
-
-    table = template.find("#sponsor_collateral_table");
-    records = _.rest($(table).find("tr"));
-    save = [];
-    $(records).each( function () {
-      var sponsor = $(this).find("a.editSponsor").html();
-      var received = $(this).find("a.editReceived").html();
-      var date = $(this).find("a.editDate").html();
-      var receivedBy = $(this).find("a.editReceivedBy").html();
-      if(!($(this).find("a.editable-empty.editSponsor").length > 0))
-      {
-        if(received == "Empty") { received = ""; }
-        if(date == "Empty") { date = ""; }
-        if(receivedBy == "Empty") { receivedBy = ""; }
-        var push = {
-          sponsor: sponsor,
-          received: received,
-          date: date,
-          receivedBy: receivedBy
-        };
-        save.push(push);
-      }
+        toastr.success('Online promotions info saved!', 'Week 3')
     });
 
-    Meteor.call("updateSponsorCollateral",
-    {
-      sponsorCollateral: save,
-      selected: Session.get("selected")
-    },
-    function (error, _id) {
-      if(error) {
-        toastr.error(error.details, error.reason)
-      }
-      else
-        toastr.success('Updated sponsor collaterals info!', 'Week 4')
-    })
 
-    table = template.find("#posting_assignments_table");
-    records = _.rest($(table).find("tr"));
-    save = [];
-    $(records).each( function () {
-      var person = $(this).find('a.editPerson').html();
-      if(person == "Empty")
-        save.push("")
-      else
-        save.push(person)
-    });
-
-    Meteor.call("updatePostingAssignments",
-    {
-      postingAssignments: save,
-      selected: Session.get("selected")
-    },
-    function (error, _id) {
-      if(error) {
-        toastr.error(error.details, error.reason)
-      }
-      else
-        toastr.success('Posting assignments saved!', 'Week 4')
-    })
-
-    table = template.find("#rtr_table");
-    records = _.rest($(table).find("tr"));
-    save = [];
-    $(records).each( function () {
-      var tth = $($(this).find('a.editPerson')[0]).html();
-      var wf = $($(this).find('a.editPerson')[1]).html();
-      if(tth == "Empty")
-        tth = ""
-      if(wf == "Empty")
-        wf = "" 
-      save.push({tth: tth, wf: wf})
-    });
-
-    Meteor.call("updateRTR",
-    {
-      rtr: save,
-      selected: Session.get("selected")
-    },
-    function (error, _id) {
-      if(error) {
-        toastr.error(error.details, error.reason)
-      }
-      else
-        toastr.success('RTR info saved!', 'Week 4')
-    })
-
-    table = template.find("#other_table");
-    records = _.rest($(table).find("tr"));
-    save = [];
-    $(records).each( function () {
-      var promotion = $(this).find("a.editPromotion").html();
-      var status = $(this).find("a.editStatus").html();
-      if($(this).find("a.editable-empty").length == 0)
-      {
-        var push = {
-          promotion: promotion,
-          status: status
-        }
-        save.push(push);        
-      }
-    });
-
-    Meteor.call("updateOtherPromotions",
-    {
-      otherPromotions: save,
-      selected: Session.get("selected")
-    },
-    function (error, _id) {
-      if(error) {
-        toastr.error(error.details, error.reason)
-      }
-      else
-        toastr.success('Other promotions info saved!', 'Week 4')
-    })
     Meteor.call("updateText",
     {
       selected: Session.get("selected")
