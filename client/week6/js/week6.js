15339da3671192a9af767febde76c6b95e1c7128
Karlo <karlo@Karlos-MacBook-Air.local>
2013-06-04 12:18:25 +0800

    addeed week6

--- client/week6/js/week6.js
+++ client/week6/js/week6.js
@@ -1,14 +1,14 @@
-Template.week5.dates = function () {
+Template.week6.dates = function () {
   var event = Events.findOne({_id: Session.get("selected")});
   if(event)
   {
-    var startDate = moment(event.startTime).subtract('weeks', 2).format("ddd, MMM DD");
-    var endDate = moment(event.startTime).subtract('weeks', 1).format("ddd, MMM DD");
+    var startDate = moment(event.startTime).subtract('weeks', 1).format("ddd, MMM DD");
+    var endDate = moment(event.startTime).format("ddd, MMM DD");
     return startDate + " - " + endDate;
   }
 }
 
-Template.week5.events({
+Template.week6.events({
   'click #save' : function (event, template) {
     Session.set("loading", true)    
     var table = template.find("table#sponsor_collateral_table");
@@ -43,7 +43,7 @@ Template.week5.events({
         toastr.error(error.details, error.reason)
       }
       else
-        toastr.success('Updated sponsor collaterals info!', 'Week 5')
+        toastr.success('Updated sponsor collaterals info!', 'Week 6')
     })
     table = template.find("table#remind_performers_table");
     records = _.rest($(table).find("tr"));
@@ -60,7 +60,7 @@ Template.week5.events({
         save.push(push);
       }
     });
-    Meteor.call("updatePerformerRemind",
+    Meteor.call("updatePerformerRemind2",
     {
       performerRemind: save,
       selected: Session.get("selected")
@@ -72,56 +72,7 @@ Template.week5.events({
       else
         toastr.success("Remind performers info saved!", "Week 5")
     });
-
-    var saveHeader = [];
-    table = template.find("#work_assignments_table");
-    records = _.first($(table).find("tr"));
-    $(_.rest($(records).children())).each( function () {
-      if($(this).find('a.editable-empty').length == 0)
-        {
-          var push = {
-            start: $(this).find('a.editShiftStartTime').html(),
-            end: $(this).find('a.editShiftEndTime').html()
-          };
-          saveHeader.push(push); 
-        }
-    });
-
-    var saveWork = [];
-    records = _.rest($(table).find("tr"));      
-    $(records).each( function () {
-      var work = $(this).find("a.editWorkAssignments").html();
-      var count = 0;
-      if($(this).find("a.editable-empty.editWorkAssignments").length == 0)
-        {
-          var push = {name: work, workers: []};
-          $(_.rest($(this).children())).each( function () {
-            if(count != saveHeader.length)
-              {
-                if($(this).find("a.editable-empty").length > 0)
-                  push.workers.push("");
-                else
-                  push.workers.push($(this).find("a.editWorkers").html());
-                count++;
-              }
-          });
-          saveWork.push(push);
-        }
-    });
-
-    Meteor.call("updateWorkAssignments",
-    {
-      workAssignments: saveWork,
-      workAssignmentsHeader: saveHeader,
-      selected: Session.get("selected"),
-    },
-    function (error, _id) {
-      if (error) {
-        toastr.error(error.details, error.reason)
-      }
-      else
-        toastr.success('Work assignments saved!', 'Week 5')
-    });
+    
     table = template.find("#posting_assignments_table");
     records = _.rest($(table).find("tr"));
     save = [];
@@ -159,7 +110,7 @@ Template.week5.events({
       save.push({tth: tth, wf: wf})
     });
 
-    Meteor.call("updateRTR",
+    Meteor.call("updateRTR2",
     {
       rtr: save,
       selected: Session.get("selected")
