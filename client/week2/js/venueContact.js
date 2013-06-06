7175a0489e1fa476609a5db652a672cea7fd38cf
Karlo <karlo@Karlos-MacBook-Air.local>
2013-06-06 12:39:15 +0800

    added add row button for venue contact table

---
+++ client/week2/js/venueContact.js
@@ -0,0 +1,25 @@
+Template.venueContact.events({
+  'click #addEntry' : function(event, template) {
+    var table = template.find("#venue_contact_table");
+    $(table).append('<tr><td><a href="#" class="editVenueContact"></a></td><td><a href="#" class="editVenueStatus">Not yet contacted</a></td></tr>');
+    $(template.findAll(".editVenueContact")).editable({
+      unsavedclass: null
+    });
+    $(template.findAll(".editVenueStatus")).editable({
+      unsavedclass: null,
+      type: 'select',
+      source: ["Not yet contacted", "Contacted", "Approved", "Rejected"]
+    });
+  }
+});
+
+Template.venueContact.rendered = function () {
+  $(this.findAll(".editVenueContact")).editable({
+    unsavedclass: null
+  });
+  $(this.findAll(".editVenueStatus")).editable({
+    unsavedclass: null,
+    type: 'select',
+    source: ["Not yet contacted", "Contacted", "Approved", "Rejected"]
+  });
+}
