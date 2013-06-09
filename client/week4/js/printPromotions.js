2306749345c46a8379a8406b0374e6f9684468cf
Karlo <karlo.karlo.karlo@gmail.com>
2013-06-09 10:36:34 +0800

    Added add row button

---
+++ client/week4/js/printPromotions.js
@@ -0,0 +1,14 @@
+Template.printPromotions.events({
+  'click #addEntry' : function (event, template){
+    var table = template.find("#printPromotions");
+    $(table).append('<tr><td><a href="#" class="editTask"></a></td><td><a href="#" class="editStatus"></a></td></tr>');
+    $(template.findAll(".editTask")).editable({
+      unsavedclass: null
+    });
+    $(template.findAll(".editStatus")).editable({
+      type: 'select',
+      source: ["Not yet started", "Started", "Completed"],
+      unsavedclass: null
+    });
+  }
+})
